3e4f1ddaf7b89a8eb0d55a735e5f30c3de223733
Michael Hinton <michael.g.hinton@gmail.com>
2019-11-03 20:16:14 -0700

    4.3: Got paddles moving

--- src/systems/paddle.rs
+++ src/systems/paddle.rs
@@ -28,14 +28,18 @@ impl<'s> System<'s> for PaddleSystem {
                 Side::Left => input.axis_value("left_paddle"),
                 Side::Right => input.axis_value("right_paddle"),
             };
+            // if let Some(mv_amount) = movement {
+            //     if mv_amount != 0.0 {
+            //         let side_name = match paddle.side {
+            //             Side::Left => "left",
+            //             Side::Right => "right",
+            //         };
+            //         println!("Side {:?} moving {}", side_name, mv_amount);
+            //     }
+            // }
             if let Some(mv_amount) = movement {
-                if mv_amount != 0.0 {
-                    let side_name = match paddle.side {
-                        Side::Left => "left",
-                        Side::Right => "right",
-                    };
-                    println!("Side {:?} moving {}", side_name, mv_amount);
-                }
+                let scaled_amount = 1.2 * mv_amount as f32;
+                transform.prepend_translation_y(scaled_amount);
             }
         }
     }
